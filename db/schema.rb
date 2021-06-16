a8906f723cb62c23a4b581cf73d6f24e34d305a4
hirane <diego.preciado@alumno.udg.mx>
2021-06-16 09:29:01 -0500

    playlist edit, index,form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_144844) do
+ActiveRecord::Schema.define(version: 2021_06_16_141915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_144844) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.boolean "public", default: false
+    t.text "description"
   end
 
   create_table "song_playlists", force: :cascade do |t|
